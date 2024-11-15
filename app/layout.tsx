87ba30266ec3668dbae11b3e3ccc9d9868d1bb64
wujunhua <wujunhua@uinnova.com>
2024-11-15 19:03:35 +0800

    updateui

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <I18nProvider>
-          <div className="p-4">
+          <div className="fixed top-4 right-4 z-50">
             <LanguageSwitcher />
           </div>
           {children}
